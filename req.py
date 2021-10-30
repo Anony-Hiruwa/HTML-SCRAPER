c5b169e1e057680b167d1f6335ea61687ead2bbf
W.Hirusha Rukshan Fernando <87813313+Dark-Knight-Hiruwa@users.noreply.github.com>
2021-10-30 06:42:00 +0530

    😈😈

--- req.py
+++ req.py
@@ -1,5 +1,20 @@
+#CODED BY HIRUWA
+
+
 import os
+import time
+
+print('INSTALLING DEPENDENCIES.....')
+print('wait.....')
+time.sleep(2)
 path = "/storage/emulated/0/"
 os.chdir(path)
 nfol = "HTML"
 os.makedirs(nfol)
+os.system('clear')
+print('SUCCESFULLY INSTALLED')
+time.sleep(2)
+os.system('clear')
+print('AUTOMATICALLY START HTML SCRAPER TOOL.....')
+os.system('clear')
+os.system('python html.py')
